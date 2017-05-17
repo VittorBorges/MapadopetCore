44ba4fba908f8bb6565732f499e254373c71e5b0
emilianolessa <emiliano.lessa@crptecnologia.com.br>
2017-05-17 14:01:31 -0300

    Add Admin

---
+++ MapadopetCore/Areas/Admin/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MapadopetCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DefaultController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
