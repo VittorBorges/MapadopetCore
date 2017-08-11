717cdf0fbb3767037f1b302665ef4eff0b61566e
Vittor Borges <vittor.borges@crptecnologia.com.br>
2017-08-10 22:48:47 -0300

    implementação do popup dinamico

---
+++ MapadopetCore/wwwroot/js/config/modalHome.js
@@ -0,0 +1,21 @@
+
+  var mdInserir =  { 
+    "elementos" : [     {"n":"rowPetNome", "v" : true },
+            { "n": "rowPetDescricao", "v": true },
+            { "n": "rowPetTelefone", "v": true },
+            { "n": "rowPetFoto", "v": true },
+            { "n": "imgLostPet", "v": true },
+            { "n": "rowPetLocal", "v": true }
+        ],
+      "labels": [
+        {"n": "mdNome", "v" : "Perdi meu Pet"},
+        {"n": "mdPetNome", "v": "Atende pelo nome"},
+        {"n": "mdPetNomeHelp", "v": "Qual o nome do seu pet ?"},
+        {"n": "mdPetDescricao", "v": "Descrição:"},
+        {"n": "mdPetDescricaoHelp", "v": "Descreva o seu pet, fale das cores, se estava com o pelo aparado ou não, estilo do rabo ou orelhas cortadas."},
+        {"n": "mdPetTelefone", "v": "Telefone de contato"},
+        {"n": "mdPetTelefoneHelp", "v": "Coloque um número do whatsapp para facilitar o contato !"},
+        {"n": "mdPetFoto", "v": "Foto do seu querido"},
+        { "n": "mdPetFotoHelp", "v" : "Escolha o arquivo... " }
+      ]
+  };
