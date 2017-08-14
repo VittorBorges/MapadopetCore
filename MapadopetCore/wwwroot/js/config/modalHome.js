661d3951cd80749055ea005f12f401210af47297
Vittor Borges <vittor.borges@crptecnologia.com.br>
2017-08-14 18:53:21 -0300

    alterações no mapa

--- MapadopetCore/wwwroot/js/config/modalHome.js
+++ MapadopetCore/wwwroot/js/config/modalHome.js
@@ -1,21 +1,200 @@
+var mdInserirPerdiMeuPet = {
+  "elementos": [
+    {
+      "n": "rowPetNome",
+      "v": true
+    },
+    {
+      "n": "rowPetDescricao",
+      "v": true
+    },
+    {
+      "n": "rowPetTelefone",
+      "v": true
+    },
+    {
+      "n": "rowPetFoto",
+      "v": true
+    },
+    {
+      "n": "imgLostPet",
+      "v": true
+    },
+    {
+      "n": "rowPetLocal",
+      "v": true
+    }
+  ],
+  "labels": [
+    {
+      "n": "mdNome",
+      "v": "Perdi meu Pet"
+    },
+    {
+      "n": "mdPetNome",
+      "v": "Atende pelo nome"
+    },
+    {
+      "n": "mdPetNomeHelp",
+      "v": "Qual o nome do seu pet ?"
+    },
+    {
+      "n": "mdPetDescricao",
+      "v": "Descrição"
+    },
+    {
+      "n": "mdPetDescricaoHelp",
+      "v": "Descreva o seu pet, fale das cores, se estava com o pelo aparado ou não, estilo do rabo ou orelhas cortadas."
+    },
+    {
+      "n": "mdPetTelefone",
+      "v": "Telefone de contato"
+    },
+    {
+      "n": "mdPetTelefoneHelp",
+      "v": "Coloque um número do whatsapp para facilitar o contato !"
+    },
+    {
+      "n": "mdPetFoto",
+      "v": "Foto do seu querido"
+    },
+    {
+      "n": "mdPetFotoHelp",
+      "v": "Escolha o arquivo... "
+    }
+  ]
+};
 
-  var mdInserir =  { 
-    "elementos" : [     {"n":"rowPetNome", "v" : true },
-            { "n": "rowPetDescricao", "v": true },
-            { "n": "rowPetTelefone", "v": true },
-            { "n": "rowPetFoto", "v": true },
-            { "n": "imgLostPet", "v": true },
-            { "n": "rowPetLocal", "v": true }
-        ],
-      "labels": [
-        {"n": "mdNome", "v" : "Perdi meu Pet"},
-        {"n": "mdPetNome", "v": "Atende pelo nome"},
-        {"n": "mdPetNomeHelp", "v": "Qual o nome do seu pet ?"},
-        {"n": "mdPetDescricao", "v": "Descri��o:"},
-        {"n": "mdPetDescricaoHelp", "v": "Descreva o seu pet, fale das cores, se estava com o pelo aparado ou n�o, estilo do rabo ou orelhas cortadas."},
-        {"n": "mdPetTelefone", "v": "Telefone de contato"},
-        {"n": "mdPetTelefoneHelp", "v": "Coloque um n�mero do whatsapp para facilitar o contato !"},
-        {"n": "mdPetFoto", "v": "Foto do seu querido"},
-        { "n": "mdPetFotoHelp", "v" : "Escolha o arquivo... " }
-      ]
-  };
+  var mdInserirAbandonado = {
+  "elementos": [
+    {
+      "n": "rowPetNome",
+      "v": false
+    },
+    {
+      "n": "rowPetDescricao",
+      "v": true
+    },
+    {
+      "n": "rowPetTelefone",
+      "v": false
+    },
+    {
+      "n": "rowPetFoto",
+      "v": true
+    },
+    {
+      "n": "imgLostPet",
+      "v": true
+    },
+    {
+      "n": "rowPetLocal",
+      "v": true
+    }
+  ],
+  "labels": [
+    {
+      "n": "mdNome",
+      "v": "Vi um pet abandonado"
+    },
+    {
+      "n": "mdPetNome",
+      "v": ""
+    },
+    {
+      "n": "mdPetNomeHelp",
+      "v": ""
+    },
+    {
+      "n": "mdPetDescricao",
+      "v": "Descreva o pet"
+    },
+    {
+      "n": "mdPetDescricaoHelp",
+      "v": "Descreva o pet que foi visto abandonado"
+    },
+    {
+      "n": "mdPetTelefone",
+      "v": ""
+    },
+    {
+      "n": "mdPetTelefoneHelp",
+      "v": ""
+    },
+    {
+      "n": "mdPetFoto",
+      "v": "Foto do amigão perdido"
+    },
+    {
+      "n": "mdPetFotoHelp",
+      "v": "Escolha o arquivo... "
+    }
+  ]
+};
+
+  var mdInserirAdocao = {
+  "elementos": [
+    {
+      "n": "rowPetNome",
+      "v": true
+    },
+    {
+      "n": "rowPetDescricao",
+      "v": true
+    },
+    {
+      "n": "rowPetTelefone",
+      "v": true
+    },
+    {
+      "n": "rowPetFoto",
+      "v": true
+    },
+    {
+      "n": "imgLostPet",
+      "v": true
+    },
+    {
+      "n": "rowPetLocal",
+      "v": true
+    }
+  ],
+  "labels": [
+    {
+      "n": "mdNome",
+      "v": "Perdi meu Pet"
+    },
+    {
+      "n": "mdPetNome",
+      "v": "Atende pelo nome"
+    },
+    {
+      "n": "mdPetNomeHelp",
+      "v": "Qual o nome do seu pet ?"
+    },
+    {
+      "n": "mdPetDescricao",
+      "v": "Descrição:"
+    },
+    {
+      "n": "mdPetDescricaoHelp",
+      "v": "Descreva o seu pet, fale das cores, se estava com o pelo aparado ou não, estilo do rabo ou orelhas cortadas."
+    },
+    {
+      "n": "mdPetTelefone",
+      "v": "Telefone de contato"
+    },
+    {
+      "n": "mdPetTelefoneHelp",
+      "v": "Coloque um n&uacute;mero do whatsapp para facilitar o contato !"
+    },
+    {
+      "n": "mdPetFoto",
+      "v": "Foto do seu querido"
+    },
+    {
+      "n": "mdPetFotoHelp",
+      "v": "Escolha o arquivo... "
+    }
+  ]
+};
